3be7784725bca44a614390f59876605c3cb28f28
Ngan Kim Khong <ngankhong03@gmail.com>
2020-01-14 05:09:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,8 @@
 def roll_call_dwarves(array)
   # Your code here
-  array.each_with_index
+  array.each_with_index{ |item, index| 
+  hashing[item] = index 
+} 
 end
 
 def summon_captain_planet# code an argument here
