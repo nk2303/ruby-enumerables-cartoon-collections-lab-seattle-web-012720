7f3224b611aacfa978993a55e26617945f091ab0
Ngan Kim Khong <ngankhong03@gmail.com>
2020-01-14 05:37:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,8 +16,8 @@ def long_planeteer_calls(array)
   array.any? {|i| i.length > 4}
 end
 
-def find_the_cheese()
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  
+  array.include?
 end
