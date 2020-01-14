235ee6aaf1d1929b9a3e9a0ee9b5eb117ee5a40f
Ngan Kim Khong <ngankhong03@gmail.com>
2020-01-14 05:25:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,9 +5,9 @@ def roll_call_dwarves(array)
   } 
 end
 
-def summon_captain_planet(array)capitalize
+def summon_captain_planet(array)
   array.map! { |item| 
-    puts "#{index + 1}.*#{item}"
+    item.capitalize
   } 
 end
 
